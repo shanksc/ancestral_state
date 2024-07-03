9d20e6c63db2367c33fd1346a997f232f004eea1
shanksc <cwshanks@ucsc.edu>
2024-07-03 11:01:09 -0700

    added indels

--- calc_ancestor_allele.smk
+++ calc_ancestor_allele.smk
@@ -13,13 +13,17 @@ print(config['ancestors'])
 print(config['contig-info'])
 #print(config['compare'])
 
+
+chroms=list(range(1,23)) + ['chrX', 'chrY']
 #pass config file
 rule all:
     input:
         #f"{config['working']}/{config['ref']}.ready.txt",
-        expand('{prefix}_{autosome}.txt', autosome=range(1,23), prefix=f"{config['working']}/{config['ref']}"),
-        expand('{prefix}_chr{autosome}.fa', autosome=range(1,23), prefix=f"{config['working']}/{config['ref']}"),
-        expand('{prefix}_chr{autosome}_stats.txt', autosome=range(1,23), prefix=f"{config['working']}/{config['ref']}")
+        #expand('{prefix}_{autosome}.txt', autosome=range(1,23), prefix=f"{config['working']}/{config['ref']}"),
+        expand('{prefix}_chr{autosome}.fa', autosome=chroms, prefix=f"{config['working']}/{config['ref']}"),
+        #add rule to produce the felsing version, expand the config to support this
+        #get stats for each of these as well 
+	#expand('{prefix}_chr{autosome}_stats.txt', autosome=chroms, prefix=f"{config['working']}/{config['ref']}")
 
 #computing only autosomes
 #create empty stats files for each chr 
@@ -38,18 +42,19 @@ rule create_stats:
             open(autosome_file, 'w').close()
         open(output[0], 'w').close()
 
-
-rule calc_anc:
+#create hg38 based fastas of SNPs 
+rule calc_anc_snps:
     input:  
-        "{prefix}_{autosome}.txt",
+        #"{prefix}_{autosome}.txt",
         config['contig-info']
     output: 
         "{prefix}_chr{autosome}.fa"
+    	#"{prefix_chr{autosome}_indels.vcf"
     shell:
         """
         python3 scripts/create_ancestor.py --taf {config[taf]} --target {config[ref]}.chr{wildcards.autosome}\
         --ancs {config[ancestors][anc3]} {config[ancestors][anc2]} {config[ancestors][anc1]}\
-        --contigs-info {config[contig-info]} --o {output}
+        --contigs-info {config[contig-info]} --o {wildcards.prefix}_chr{wildcards.autosome}
         """
 
 rule compute_stats:
@@ -60,4 +65,3 @@ rule compute_stats:
     shell:
         "bash scripts/get_base_counts.sh {input} > {output}"
 
-#rule 
--- scripts/create_ancestor.py
+++ scripts/create_ancestor.py
@@ -3,8 +3,9 @@ import re
 import time
 import sys
 from taffy.lib import AlignmentReader, TafIndex
-#from Bio import SeqIO
-#may want to just use block obj to simplify later
+
+
+#ancseq of snps for reference
 class AncSeq:
     def __init__(self, seq, start, end, strand):
         self.seq = seq 
@@ -12,6 +13,14 @@ class AncSeq:
         self.end = end 
         self.strand = strand
         #may want to add strand direction
+
+#variants class for writing out vcf 
+class Variant:
+    def __init__(self, chrom):
+        self.chrom = chrom
+        self.ref = ''
+        self.alt = '' 
+        self.start = None
 
         
 def get_args():
@@ -22,7 +31,7 @@ def get_args():
     parser.add_argument('--ancs', type=str, required=True, nargs='+', help='Anc4.Anc4.refChr, Anc3.Anc3.refChr etc.')
     #parser.add_argument('--chr', type=int, required=True, help='chr corresponding to taf')
     parser.add_argument('--contigs-info', type=str, required=True, help='tsv containing contig, size, chr corresponding to reference (hg38/CHM13)')
-    parser.add_argument('--o', type=str, required=True, help='fasta to output for contig')
+    parser.add_argument('--o', type=str, required=True, help='indel vcf, fasta to output for contig')
 
     return parser.parse_args()
 
@@ -85,7 +94,7 @@ we look at the human-chimp (a) ancestral sequence and compare it to the chimp
 (b) and to the human-chimp-orang (c) ancestral sequences. We use the following
 convention for the ancestral sequence:
 
-In our case let (a) be the human-chimp-bonobo, (b) human-chimp-bonobo-gorilla, (c) human-chimp-bonobo-gorilla-orangutan
+In our case let (a) be the human-chimp-bonobo, (b) chimp-bonobo (c) human-chimp-bonobo-gorilla
 
 uppercase when all 3 –(a), (b) and (c)– sequences agree
 lowercase when:
@@ -97,10 +106,7 @@ N when both (b) and (c) disagree with (a)
 - (dash) when no there is no ancestral allele, this is a lineage-specific insertion
 . (dot) when there is no alignment, i.e. no data.
 '''
-def get_anc_allele(col, ancs, anc_to_indices):
-    #ancs is in order of a, b, c 
-    a, b, c = [col[anc_to_indices[anc]] for anc in ancs]
-
+def get_consensus(a, b, c):
     #unanimous 
     if a == b == c:
         return a.upper()
@@ -112,21 +118,32 @@ def get_anc_allele(col, ancs, anc_to_indices):
         return 'N'
     #lineage specific insertion
     return '-'
-    
 
+#this breaks if MAF/TAF isn't properly filtered to contain single copies of contigs 
+def get_anc_allele(col, ancs, anc_to_indices):
+    #ancs is in order of a, b, c 
+    a, b, c = [col[anc_to_indices[anc]] for anc in ancs]
+
+    return get_consensus(a, b, c)
+
+
+#do we need to realign seqs for context like Ensembl?
 #since we need an hg38 based fasta, we need only original aligned bases
 def build_seq(anc_alleles, row):
     
     #get indices or original bases
     indices = []
     bases = row.bases()
+    start = row.start()
+
+    #these are original hg38/chm13 bases
     for i, base in enumerate(bases):
         if base != '-':
             indices.append(i)
     
     seq = ''.join([anc_alleles[idx] for idx in indices])
+    #row.length() is the number of bases in the row of our reference
     assert len(seq) == row.length()
-
     return seq
 
 
@@ -136,23 +153,100 @@ def remove_last_numbers(input_string):
     return re.sub(r'\d+$', '', input_string)
 
 
+def get_alleles(chrom, row, block, ancs, anc_to_indices):
+    #ref will always be first in col
+    curr_var = Variant(chrom=chrom)
+    ref_bases = row.bases()
+    indels = []
+    anc_alleles = []
+    
+    #init ref_position to first valid_base in block
+    for idx, base in enumerate(ref_bases):
+        if base != '-':
+            ref_position = row.start() + idx + 1
+            break
+
+    assert len(ref_bases) == block.column_number()
+    for i in range(0, block.column_number()):
+        col = block.get_column(i)
+        a, b, c = [col[anc_to_indices[anc]] for anc in ancs]
+        
+
+
+        anc_allele = get_consensus(a, b, c)
+        anc_alleles.append(anc_allele)
+
+        #now classify as indel or snp relative to ref 
+
+        #anc allele is insertion relative to ref - should we go beyond 1bp (why not)?
+        #if ref_bases[i] == '-' and anc_allele != 'N' and anc_allele != '-':
+        #filter insertions and deletions later
+        if ref_bases[i] == '-':
+            #build insertion
+            #print('insert')
+            #print(ref_bases[i-3:i+1])
+            #print(anc_allele)
+            if curr_var.ref == '':
+                curr_var.start = ref_position - 1#check that is based correctly
+                curr_var.ref = ref_bases[i-1]
+                curr_var.alt += ref_bases[i-1] + anc_allele
+            #longer than 1bp 
+            else:
+                curr_var.alt += anc_allele
+
+        #anc allele is deletion relative to ref
+        elif ref_bases[i] != '-' and anc_allele == '-':
+            #print(ref_bases[i-3:i+1])
+            if curr_var.ref == '':
+                #make 1-based
+                curr_var.start = row.start() + i - 1
+                curr_var.ref = ref_bases[i-1] + ref_bases[i]
+                curr_var.alt = ref_bases[i-1]
+                #longer than 1bp
+                #print(ref_bases[i-3:i+3])
+                #print(curr_var.start, curr_var.ref, curr_var.alt)
+                #sys.exit()
+            else:
+                curr_var.ref += ref_bases[i]
+
+        #indel ends if there aren't any more gaps
+        elif curr_var.ref != '':
+            indels.append(curr_var)
+            #print(curr_var.ref, curr_var.alt, curr_var.start)
+            #sys.exit()
+            curr_var = Variant(chrom=chrom)
+        
+        if ref_bases[i] != '-':
+            ref_position += 1
+
+    return indels, anc_alleles
+            
+
 #iterate through blocks, getting the inferred ancestral sequence and the interval of the sequence
 #we may want to refactor this to just use the block obj later.
 def get_ancestral_seqs(taf_file, target, ancs, size):
 
     taf_index = TafIndex(taf_file + '.tai', is_maf=False)
 
+    start = 0
+    #chrom = target.split('.')[1]
+    chrom = target.split('.')[1]
+    #chrom = re.search(r'\d+$', target.split('.')[1]).group(0)
+    print(f'chr: {chrom}')
+
     #this is a temporary fix to deal with the taffy view error where the first chromosome isn't found in the index
     #this is fixed in newest taffy update 
-    start=0
     if target == 'hg38.chr1':
         start = 10000
     
     with AlignmentReader(taf_file, taf_index=taf_index, sequence_name=target, start=start, length=size) as mp:
-        print('iterating', flush=True)
+        #print('iterating', flush=True)
         ancestral_seqs = []
         total_blocks = 0
         valid_blocks = 0
+
+        indels = []
+
         for block in mp:
             
             #The first row of each alignment block consists of a sequence from the reference genome.
@@ -172,19 +266,18 @@ def get_ancestral_seqs(taf_file, target, ancs, size):
                     if prefix in ancs:
                         anc_to_indices[prefix] = i
 
-                #print(col_names)
-                #print(anc_to_indices)
-                anc_alleles = []
-                for i in range(block.column_number()):
-                    anc_alleles.append(get_anc_allele(block.get_column(i), ancs, anc_to_indices))
-                    #anc_alleles.append(get_anc_allele(block.get_column(i), ancs, anc_to_indices, row))
-                #print(anc_alleles)
+
+                new_indels, anc_alleles = get_alleles(chrom, row, block, ancs, anc_to_indices)
+                if new_indels:
+                    indels.extend(new_indels)
+                
                 anc_seq = AncSeq(seq=build_seq(anc_alleles, row), start=row.start(), end=row.start()+row.length(), strand=row.strand())
+                #we can change this to yield 
                 ancestral_seqs.append(anc_seq)
 
     print(f'valid blocks: {valid_blocks}')
     print(f'total blocks: {total_blocks}')
-    return ancestral_seqs
+    return indels, ancestral_seqs
 
 #ancestral seqs is ordered
 #return a generator 
@@ -222,11 +315,26 @@ def get_fasta(anc_seqs, size, line_length=80):
     
     print(f'total chars after blocks {total_chars}')
 
+#write out indels in vcf format
+def write_vcf(indels, out):
+
+    #write header
+    with open(out+'.vcf', 'w') as f:
+        f.write('##fileformat=VCFv4.2\n')
+        
+        #id will always be blank until we get rs numbers from dbSNP
+        f.write('#CHROM\tPOS\tID\tREF\tALT\n')
+        for indel in indels:
+            if '-' not in indel.alt and 'N' not in indel.alt:
+                #check for 1bp indels
+                if (len(indel.ref) == 2 and len(indel.alt) == 1) or (len(indel.ref) == 1 and len(indel.alt) == 2):
+                    f.write(f'{indel.chrom}\t{indel.start}\t.\t{indel.ref}\t{indel.alt}\n')
+
 
 def write_fasta(anc_seqs, target, size, out, line_length=80):
     
     total_chars = 0
-    with open(out, 'w') as f:
+    with open(out+'.fa', 'w') as f:
         f.write(f'>{target}\n')
         buffer = ''
 
@@ -255,20 +363,27 @@ def main():
     print(f'{args.target} size: {size}')
     
     start_time = time.time()
-    anc_seqs = get_ancestral_seqs(args.taf, args.target, args.ancs, size)
+    indels, anc_seqs = get_ancestral_seqs(args.taf, args.target, args.ancs, size)
     end_time = time.time()
     
     print(f'time: {end_time - start_time}')
-
+    
+    print(indels[0].ref, indels[0].alt, indels[0].start)
+    
     #check that it's sorted
     assert(sorted(anc_seqs, key = lambda x:x.start) == anc_seqs)
     #check for only positive strands in reference
     assert all(s.strand for s in anc_seqs)
 
     print(f'max end coord in taf {max(s.end for s in anc_seqs)}')
+    
 
+    #should be no difference from our other fasta 
     total_chars = write_fasta(anc_seqs, args.target, size, args.o)
     print(f'{total_chars} sequence characters written to fasta')
+
+    #write out vcf with indels
+    write_vcf(indels, args.o)
 
 if __name__ == '__main__':
     main()
